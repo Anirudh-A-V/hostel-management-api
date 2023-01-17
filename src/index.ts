3dd28fa4986aad34cb39b560bc2157d73a7b3528
Anirudh A V <anirudh.av02@gmail.com>
2023-01-17 11:18:44 +0530

    feat: initialised express app

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
+import express from 'express';
+import bodyparser from 'body-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
+const app = express();
+app.use(bodyparser.json());
+app.use(cors());
 
-console.log('Hello World');
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
 
