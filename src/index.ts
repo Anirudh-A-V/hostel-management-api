87eec42d1cfb4b2f102118fa1e3227f39f8b54c1
Anirudh A V <anirudh.av02@gmail.com>
2023-01-19 23:37:08 +0530

    feat: winston initialised

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
+import logger from './middleware/Logger.js';
+
 const app = express();
 app.use(bodyparser.json());
 app.use(cors());
@@ -17,15 +19,23 @@ app.get('/', (req: Request, res: Response) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
 });
 
 
 mongoose.set('strictQuery', true);
 mongoose.connect(`${process.env.MY_CONNECTION_URL}`)
     .then(() => {
-        console.log("Connected to MongoDB");
+        logger.info("Connected to MongoDB");
     })
     .catch((err) => {
-        console.log("Error connecting to MongoDB", err);
+        logger.error("Error connecting to MongoDB", err);
     });
+
+// @TODO - Add logging
+// @TODO - Add error handling
+// @TODO - Add routes
+// @TODO - Add authentication
+// @TODO - Add authorization
+// @TODO - Add testing
+// @TODO - Add documentation 
