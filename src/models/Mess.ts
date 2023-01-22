ee649073e624dfc19525c775ee3d9b40911d1fb9
Anirudh A V <anirudh.av02@gmail.com>
2023-01-22 19:50:54 +0530

    refactor: Mess model updated

--- src/models/Mess.ts
+++ src/models/Mess.ts
@@ -23,10 +23,10 @@ const messSchema = new Schema({
         required: true,
         unique: true
     },
-    type: {
-        type: String,
-        required: true,
-    }
+    price: {
+        type: Number,
+        required: true
+    },
 });
 
 export default mongoose.model('Mess', messSchema);
